b9933f2cf5eab43bf3195a200d0dd27d42f0e9f7
Christina <christina.phillips9838@gmail.com>
2023-03-26 12:38:25 -0400

    Finished 18.3.NOTE: May need to edit a line in vm.rs

--- src/vm.rs
+++ src/vm.rs
@@ -1,9 +1,11 @@
+use core::num;
+
 use crate::chunk::*;
 use crate::debug::*;
 use crate::value::*;
 use crate::compiler::*;
 pub struct VM {
-    // chunk : Chunk,
+    chunk : Chunk,
     ip : usize,
     stack : Vec<Value>,
     compiler : Compiler,
@@ -16,9 +18,21 @@ pub enum InterpretResult {
     InterpretRuntimeError
 }
 
+/*
+Helper struct for runtime_error function
+Add what types you need to pass for the args parameter in RuntimeError function here
+*/
+#[derive(Debug)]
+pub struct RuntimeErrorValues{
+    char: char,
+}
+
 impl VM {
     pub fn new() -> Self {
+        let code: Vec<u8> = Vec::new();
+        let lines: Vec<usize> = Vec::new();
         VM {
+            chunk: Chunk { code: code, constants: ValueArray::new(), lines: lines },
             ip : 0,
             stack : Vec::new(),
             compiler : Compiler::new(),
@@ -53,6 +67,45 @@ impl VM {
         chunk.constants.values[curr_byte as usize]
     }
 
+    pub fn binary_op(&mut self, valueType: fn(f64) -> Value, op: OpCode) -> InterpretResult{
+        loop{
+            if !is_number(self.peek(0)) || !is_number(self.peek(1)){
+                let args: Vec<RuntimeErrorValues> = Vec::new();
+                self.runtime_error("Operands must be numbers.".to_string(), args);
+                return InterpretResult::InterpretRuntimeError;
+            }
+            let b = as_number(self.pop());
+            let a = as_number(self.pop());
+            match op{
+                OpCode::OpAdd => self.push(valueType(a + b)),
+                OpCode::OpSubtract => self.push(valueType(a - b)),
+                OpCode::OpMultiply => self.push(valueType(a * b)),
+                OpCode::OpDivide => self.push(valueType(a / b)),
+                _ => ()
+            }
+        }
+    }
+
+    /*
+    Need to pass an arbitrary amount of arguments to this function,
+    so made args a vector that hold the struct RuntimeErrorValues.
+    You can add what values you may need when calling this function to the struct RuntimeErrorValues above
+     */
+    pub fn runtime_error(&mut self, format: String, args: Vec<RuntimeErrorValues>){
+        if args.len() > 0{
+            eprintln!("{} {:?}", format, args);
+        }else{
+            eprintln!("{}", format)
+        }
+        
+        //need to get index of code that corresponds to where the line of the code is stored in bytecode
+        let source_code: usize = usize::try_from(self.chunk.code[0] - 1).unwrap(); //TODO: get correct index for self.chunk.code
+        let instruction: usize = self.ip - source_code;
+        let line: i32 = self.chunk.lines[instruction].try_into().unwrap();
+        eprintln!("[line {}] in script", line);
+        self.stack.clear();
+    }
+
     fn run(&mut self, chunk: &Chunk) -> InterpretResult {
        loop {
             #[cfg(feature = "debug_trace_execution")]
@@ -66,28 +119,25 @@ impl VM {
                     // break ?
                 },
                 OpCode::OpAdd => {
-                    let b : Value = self.pop();
-                    let a : Value = self.pop();
-                    self.push(a + b);
+                    self.binary_op(number_val, OpCode::OpAdd);
                 },
                 OpCode::OpSubtract => {
-                    let b : Value = self.pop();
-                    let a : Value = self.pop();
-                    self.push(a - b);
+                    self.binary_op(number_val, OpCode::OpSubtract);
                 },
                 OpCode::OpMultiply => {
-                    let b : Value = self.pop();
-                    let a : Value = self.pop();
-                    self.push(a * b);
+                    self.binary_op(number_val, OpCode::OpMultiply);
                 },
                 OpCode::OpDivide => {
-                    let b : Value = self.pop();
-                    let a : Value = self.pop();
-                    self.push(a / b);
+                    self.binary_op(number_val, OpCode::OpDivide);
                 },
                 OpCode::OpNegate => {
-                    let negated_value = -self.pop();
-                    self.push(negated_value)
+                    if !is_number(self.peek(0)){
+                        let args: Vec<RuntimeErrorValues> = Vec::new();
+                        self.runtime_error("Operand must be a number.".to_string(), args);
+                        
+                        return InterpretResult::InterpretRuntimeError;
+                    }
+                    self.push(number_val(- as_number(self.pop())));
                 },
                 OpCode::OpReturn => {
                     print_value(self.pop());
@@ -132,5 +182,10 @@ impl VM {
 
     pub fn pop(&mut self) -> Value {
         self.stack.pop().unwrap()
+    }
+
+    pub fn peek(&mut self, distance: usize) -> Value{
+        let len = self.stack.len() - 1;
+        self.stack[len - distance]
     }
 }
