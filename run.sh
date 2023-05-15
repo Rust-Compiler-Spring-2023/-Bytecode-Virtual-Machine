073048c5f789410157f6d8d74f90327351d41cea
Kushendra Ramrup <40897858+Kushendra1@users.noreply.github.com>
2023-05-14 20:30:16 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ echo "Enter file name:"
 read file_name
 touch "../test/$file_name.lox"
 echo "Created file: ../test/$file_name.lox"
-vim -c "startinsert" ../test/$file_name.lox
+vim -T dumb -c "startinsert" ../test/$file_name.lox
 cargo run "../test/$file_name.lox"
